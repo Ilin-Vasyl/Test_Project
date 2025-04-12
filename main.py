c17637ea97af11779ecfc3a93202b05e41f21c7b
Ilin-Vasyl <ibazil78@gmail.com>
2025-04-12 17:57:45 +0200

    Fix deprecated method and update app run code

--- main.py
+++ main.py
@@ -175,5 +175,5 @@ app.layout = html.Div([
 
 # Запуск приложения
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8050))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
